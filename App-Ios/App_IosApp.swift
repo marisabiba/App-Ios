c2107ca6fc3583ff20fb8318d0151e84f269c339
marisabiba <marisa.bimpaorbiba@student.hogent.be>
2025-01-04 17:19:00 +0100

    Initial Commit

---
+++ App-Ios/App_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_IosApp.swift
+//  App-Ios
+//
+//  Created by Bluez on 4.1.25.
+//
+
+import SwiftUI
+
+@main
+struct App_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
