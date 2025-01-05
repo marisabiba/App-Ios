584739ea122bcbfc90fd9ece01b903f0bf913f1a
marisabiba <marisa.bimpaorbiba@student.hogent.be>
2025-01-05 16:04:11 +0100

    Edit functionalities

---
+++ App-Ios/Views/TripDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  TripDetailsView.swift
+//  App-Ios
+//
+//  Created by Bluez on 5.1.25.
+//
+
+import SwiftUI
+
+struct TripDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TripDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TripDetailsView()
+    }
+}
